e29a11adb08642bad1b38b2fbe3182ee095e2a88
Unnamed_DEV <a22yo22io@gmail.com>
2023-09-25 18:19:46 +0200

    Added UFO

--- Pirat/Assets/Scripts/Manager/SpawnManager.cs
+++ Pirat/Assets/Scripts/Manager/SpawnManager.cs
@@ -10,9 +10,14 @@ public class SpawnManager : MonoBehaviour
 
     public float spawn_time;
 
-    public GameObject walking_rat_pf;
-    public GameObject flying_rat_pf;
+    [Header("Rotatiom")]
+    public float ratation_speed = 60f;
+    Quaternion start_rotation;
+    Quaternion rotation_destination;
 
+    public GameObject walking_rat_pf;
+
+    public Transform spawn_pos_start;
     public Transform spawn_pos;
 
     private void Awake()
@@ -22,33 +27,59 @@ public class SpawnManager : MonoBehaviour
 
     private void Start()
     {
-        for(int i=0; i < 10; i++)
+        for (int i = 0; i < 10; i++)
         {
-            Set_Spawn_Pos();
-            Spawn_Walking_Rat();
+            Start_Set_Spawn_Pos();
+            Spawn_Walking_Rat_Start();
         }
 
         StartCoroutine(Spawn_Walking_Rat_Timer());
+    }
+
+
+    private void Update()
+    {
+        if (rotation_destination != transform.rotation)
+        {
+            transform.rotation = Quaternion.Slerp(start_rotation, rotation_destination, Time.deltaTime);
+        }
+        else if (spawn_alowance)
+        {
+            Spawn_Walking_Rat();
+            spawn_alowance = false;
+        }
     }
 
     public IEnumerator Spawn_Walking_Rat_Timer()
     {
         yield return new WaitForSeconds(spawn_time);
 
-        if (spawn_alowance)
-        {
-            Set_Spawn_Pos();
-            Spawn_Walking_Rat();
-        }
+        spawn_alowance = true;
+
+        Set_Spawn_Pos();
+
         StartCoroutine(Spawn_Walking_Rat_Timer());
+    }
+
+    public void Start_Set_Spawn_Pos()
+    {
+        Quaternion rot = Quaternion.Euler(Random.Range(0, 360),
+                                                     Random.Range(0, 360),
+                                                     Random.Range(0, 360));
+
+        transform.rotation = rot;
     }
 
     public void Set_Spawn_Pos()
     {
-        Quaternion spawn_rotation = Quaternion.Euler(Random.Range(0, 360),
-                                                    Random.Range(0, 360),
-                                                    Random.Range(0, 360));
-        transform.rotation = spawn_rotation;
+        rotation_destination = Quaternion.Euler(Random.Range(0, 360),
+                                                     Random.Range(0, 360),
+                                                     Random.Range(0, 360));
+    }
+
+    public void Spawn_Walking_Rat_Start()
+    {
+        GameObject walking_rat = Instantiate(walking_rat_pf, spawn_pos_start.position, Quaternion.identity);
     }
 
     public void Spawn_Walking_Rat()
